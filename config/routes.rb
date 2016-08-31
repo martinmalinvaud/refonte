60fa960faf1f65b8a20c60503b559fcd3633bb92
martin malinvaud <martin.malinvaud@gmail.com>
2016-08-31 17:18:19 +0200

    lets finish this part

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get "event" => "pages#event"
+  get "foodtruck" => "pages#foodtruck"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

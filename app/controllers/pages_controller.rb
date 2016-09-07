c3921f37b25daa7adf8cb3b0e31c472ba4ac3864
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-07 11:08:29 +0200

    page contact ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :event, :foodtruck ]
+  skip_before_action :authenticate_user!, only: [ :home, :event, :foodtruck, :contact, :cgv ]
 
   def home
   end

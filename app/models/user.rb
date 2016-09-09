e4e94a08a1a80542ab2dfecfb0d0ffe0e645b7a6
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-09 15:14:45 +0200

    some shit to resolve in development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+
+  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  after_create :send_welcome_email
 
   private
 

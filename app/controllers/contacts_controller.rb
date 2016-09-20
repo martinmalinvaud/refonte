28b8554d2d485239fd50d3d014b11a0eec8c2b83
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-20 15:12:42 +0200

    hum

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
+    if @contact.deliver_now
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'

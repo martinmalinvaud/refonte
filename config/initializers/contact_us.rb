1919a8eb370f65c8977c33992d3ef37169ff8760
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-20 18:00:19 +0200

    test

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,26 +6,27 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  config.mailer_from = "contact@thefoodtruck.agency"
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "contact@please-change-me.com"
+  config.mailer_to = "contact@thefoodtruck.agency"
 
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = false
+  config.require_name = true
 
   # Configure the form to ask for a subject.
-  config.require_subject = false
+  config.require_subject = true
 
   # Configure the form gem to use.
   # Example:
   # config.form_gem = 'formtastic'
-  config.form_gem = nil
+  config.form_gem = 'simple_form'
+
 
   # Configure the redirect URL after a successful submission
-  config.success_redirect = '/'
+  config.success_redirect = '/contact-success'
 
   # Configure the parent action mailer
   # Example:

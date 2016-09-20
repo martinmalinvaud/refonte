fb33002ac43d17255bac9d9a15ccf2b2067c6e5e
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-20 14:37:24 +0200

    hum

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.routes.default_url_options[:host] = 'tftav2.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => 'tftav2.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false

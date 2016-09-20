1ec4886eeec870ec287a7ea3a51a116ff87a43a0
martin malinvaud <martin.malinvaud@gmail.com>
2016-09-20 15:17:49 +0200

    smtp failed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver.now
+    if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
